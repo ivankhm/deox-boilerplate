4a19f679e524d8a2c5961631a2ac7c3e117dd524
Ivan Khmelevskiy <ivan.khm@gmail.com>
2021-07-30 12:32:56 +0300

    generating selectors naive

--- src/services/generatorService/index.ts
+++ src/services/generatorService/index.ts
@@ -2,6 +2,10 @@ import * as ts from 'typescript';
 import * as fs from 'fs';
 import * as path from 'path';
 
+import * as ejs from 'ejs';
+
+import * as selectorsFile from './templates/selectors';
+
 const capitalizeFirstLetter = (string: string) => 
  string.charAt(0).toUpperCase() + string.slice(1);
 ;
@@ -11,11 +15,21 @@ const getStoreInterface = (rootNode: ts.SourceFile, storeInterfaceName: string) 
     if (child.kind === ts.SyntaxKind.InterfaceDeclaration) {
 
       const childInterface = child as ts.InterfaceDeclaration;
-      const interfaceName = childInterface.name.escapedText;
 
-      if (interfaceName.toString() === storeInterfaceName) {
-        console.log(interfaceName);
+      if (childInterface.name.escapedText.toString() === storeInterfaceName) {
         return childInterface;
+      }
+    }
+   });
+};
+
+const getStoreActionsEnum = (rootNode: ts.SourceFile, storeActionEnumName: string) => {
+  return rootNode.forEachChild(child => {
+    if (child.kind === ts.SyntaxKind.EnumDeclaration) {
+      const childEnum = child as ts.EnumDeclaration;
+
+      if (childEnum.name.escapedText.toString() === storeActionEnumName) {
+        return childEnum;
       }
     }
    });
@@ -29,6 +43,7 @@ const getStoreName =  (filePath: string) => {
 const getName = (field: ts.NamedDeclaration) => (field.name as ts.Identifier).escapedText.toString();
 
 const getInterfaceName = (storeName: string) => `${capitalizeFirstLetter(storeName)}State`;
+const getActionsEnumName = (storeName: string) => `${capitalizeFirstLetter(storeName)}ActionTypes`;
 
 const kindToInitialValueMap: Map<ts.SyntaxKind, Function> = new Map<ts.SyntaxKind, Function>([
   [ts.SyntaxKind.TypeLiteral, (type: ts.ObjectTypeDeclaration) => Object.fromEntries(type!.members.map(getInitialValues))],
@@ -52,24 +67,49 @@ const getInitialValues = (field: ts.NamedDeclaration): [string, any] => {
 export const generateDeoxFiles = async  (filePath: string) => {
   const storeName = getStoreName(filePath);
   const storeInterfaceName = getInterfaceName(storeName);
-  
-  console.log('storeName: ', storeName);
+  const storeActionsEnumName = getActionsEnumName(storeName);
 
   const declarations = fs.readFileSync(filePath).toString();
   const rootNode = ts.createSourceFile('types.ts', declarations, ts.ScriptTarget.Latest);
 
   const storeInterface = getStoreInterface(rootNode, storeInterfaceName);
+  const storeActionEnum = getStoreActionsEnum(rootNode, storeActionsEnumName);
 
   if (!storeInterface) {
     throw new Error(`Interface ${storeInterfaceName} was not found in ${filePath}.`);
   }
 
-  console.log({ storeInterface });
+  if (!storeActionEnum) {
+    throw new Error(`Enum ${storeActionEnum} was not found in ${filePath}.`);
+  }
 
   const fieldNames = storeInterface.members.map(field => getName(field));
 
-  const initiState =  Object.fromEntries(storeInterface.members.map(getInitialValues));
+  const initialState =  Object.fromEntries(storeInterface.members.map(getInitialValues));
+  
+  const rawTemplate = selectorsFile.getTemplate(); // fs.readFileSync(path.join(__dirname, './templates/selectors.template.ts.ejs')).toString();
+  const selectorsTemplate = ejs.compile(rawTemplate);
+
+  const stateSelectorName = `get${storeInterfaceName}`;
+  const ejsData = {
+    storeName,
+    storeInterfaceName,
+    types: [storeInterfaceName, 'News', 'NewsCategory'],
+    stateSelectorName,
+    selectors: fieldNames.map(fieldName => ({
+      name: `get${capitalizeFirstLetter(fieldName)}`,
+      returnType: 'any',
+      fieldPath: fieldName,
+    })),
+  };
+
+  console.log(ejsData);
   
   
+  const result = selectorsTemplate(ejsData);
+
+  fs.writeFileSync(path.join(path.dirname(filePath), 'selectors.ts'), result);
+
+  console.log(result);
   
 };
